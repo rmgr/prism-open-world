57d59948e5f1075f56368639e78be3c1c26c6c71
jake <jake@rmgr.dev>
2026-06-15 07:34:37 +0930

    Remove factions submodule

--- modules/faction/relations/fearedby.lua
+++
@@ -1,9 +0,0 @@
---- A relation representing that an entity is a foe of another entity.
---- @class FearedByRelation : Relation
---- @overload fun(): FearedByRelation
-local FearedByRelation = prism.Relation:extend("FearedByRelation")
-
-function FearedByRelation:generateInverse()
-	return prism.relations.FearsRelation()
-end
-return FearedByRelation
--- modules/faction/relations/fears.lua
+++
@@ -1,9 +0,0 @@
---- A relation representing that an entity is a foe of another entity.
---- @class FearsRelation : Relation
---- @overload fun(): FearsRelation
-local FearsRelation = prism.Relation:extend("FearsRelation")
-
-function FearsRelation:generateInverse()
-	return prism.relations.FearedByRelation()
-end
-return FearsRelation
